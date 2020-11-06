d4173bb98cc62f69370e39b077213c86b122077a
sabari <sabari.cs10@gmail.com>
2020-11-06 14:03:58 +0530

    Initial Commit

---
+++ SNShadowSDK/SNShadowSDK.h
@@ -0,0 +1,18 @@
+//
+//  SNShadowSDK.h
+//  SNShadowSDK
+//
+//  Created by Sabari on 06/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SNShadowSDK.
+FOUNDATION_EXPORT double SNShadowSDKVersionNumber;
+
+//! Project version string for SNShadowSDK.
+FOUNDATION_EXPORT const unsigned char SNShadowSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SNShadowSDK/PublicHeader.h>
+
+
